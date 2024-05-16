c007ab1df0892fc23277a3b14f615bd138086ea7
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-17 02:34:12 +0800

    Add banner

--- share/metadata.py
+++ share/metadata.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+#
+#         :::   :::  ::::::::::::::    :::    :::
+#       :+:+: :+:+:     :+:    :+:   :+:   :+: :+:
+#     +:+ +:+:+ +:+    +:+    +:+  +:+   +:+   +:+  Irfan Hakim (MIKA)
+#    +#+  +:+  +#+    +#+    +#++:++   +#++:++#++:  https://sakurajima.social/@irfan
+#   +#+       +#+    +#+    +#+  +#+  +#+     +#+   https://github.com/irfanhakim-as
+#  #+#       #+#    #+#    #+#   #+# #+#     #+#    https://gitlab.com/irfanhakim
+# ###       #################    ######     ###
+#
+# ezdl-metadata: Metadata for the ezdl tool.
+
+
 __name__="ezdl"
 __version__="0.1.1"
 __author__="irfanhakim"
