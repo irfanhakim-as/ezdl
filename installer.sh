e243ac58440d65934fbf4abc0360858c74b20f42
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-17 13:36:57 +0800

    Realign help message

--- installer.sh
+++ installer.sh
@@ -9,11 +9,11 @@ source "./share/metadata.py"
 function help() {
     echo "Usage: ${0} [options]"; echo
     echo "OPTIONS:"
-    # echo "  -c, --config-prefix <path>           Specify a config prefix"
-    echo "  -i, --install-prefix <path>          Specify an installation prefix"
-    echo "  -l, --link-install                   Perform a symlink installation"
-    echo "  -u, --uninstall                      Uninstall application"
-    echo "  -h, --help                           Print help message"; echo
+    # echo "  -c, --config-prefix <path>        Specify a config prefix"
+    echo "  -i, --install-prefix <path>       Specify an installation prefix"
+    echo "  -l, --link-install                Perform a symlink installation"
+    echo "  -u, --uninstall                   Uninstall application"
+    echo "  -h, --help                        Print help message"; echo
     echo "Report bugs to ${__source__}/issues"
 }
 
