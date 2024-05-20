5a7d67d9819177a69abc79a47b7cb9634d4ad383
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-20 18:26:59 +0800

    Reformat xitter links

--- share/parser.py
+++ share/parser.py
@@ -2,4 +2,6 @@
 def sanitiseVideoList(videoList):
     # reformat vimeo links
     videoList = [l[:l.rfind("/") + 1].replace("vimeo.com/", "player.vimeo.com/video/").rstrip("/") if l.startswith("https://vimeo.com") else l for l in videoList]
+    # reformat xitter links
+    videoList = [l.replace("x.com/", "twitter.com/").rstrip("/") if l.startswith("https://x.com") else l for l in videoList]
     return videoList
