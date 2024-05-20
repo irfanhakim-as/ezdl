883dde41951536e9ca43923e966fc07e7a3fe72e
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-05-20 23:24:37 +0800

    Pass video list args to method if applicable

--- bin/main.py
+++ bin/main.py
@@ -111,6 +111,7 @@ def getUserInput(config, **kwargs):
         itemStyle="bright",
         itemName="video",
         margin=colMargin,
+        list=getattr(args, "video", None),
     )
 
     return {
