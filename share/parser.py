f262647743dbc77ed3644f881d78d403061ce46e
Irfan Hakim <irfanhakim.as@yahoo.com>
2025-08-18 21:01:02 +0800

    Improve parsing for vimeo links

--- share/parser.py
+++ share/parser.py
@@ -11,10 +11,13 @@
 # ezdl-parser: Parsing functions written for the ezdl tool.
 
 
+import re
+
+
 # sanitise video list
 def sanitiseVideoList(videoList):
     # reformat vimeo links
-    videoList = [l[:l.rfind("/") + 1].replace("vimeo.com/", "player.vimeo.com/video/").rstrip("/") if l.startswith("https://vimeo.com") else l for l in videoList]
+    videoList = ["https://player.vimeo.com/video/%s" % re.search(r'vimeo\.com/(\d+)', l).group(1) if l.startswith("https://vimeo.com") else l for l in videoList]
     # reformat xitter links
     videoList = [l.replace("x.com/", "twitter.com/").rstrip("/") if l.startswith("https://x.com") else l for l in videoList]
     return videoList
