b8aee816f2611ec7792f0d62f0d767d68944e4a3
Irfan Hakim <irfanhakim.as@yahoo.com>
2025-08-19 12:54:49 +0800

    Strip whitespace

--- share/parser.py
+++ share/parser.py
@@ -19,7 +19,7 @@ def sanitiseVideoList(videoList):
     # reformat vimeo links
     videoList = ["https://player.vimeo.com/video/%s" % re.search(r'vimeo\.com/(\d+)', l).group(1) if l.startswith("https://vimeo.com") else l for l in videoList]
     # reformat xitter links
-    videoList = [l.replace("x.com/", "twitter.com/").rstrip("/") if l.startswith("https://x.com") else l for l in videoList]
+    videoList = [l.replace("x.com/", "twitter.com/").strip().rstrip("/") if l.startswith("https://x.com") else l for l in videoList]
     # clean instagram links
     videoList = [l.split("?")[0].strip().rstrip("/") if "instagram.com" in l else l for l in videoList]
     return videoList
