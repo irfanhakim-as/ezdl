b7180c227614526d0694e6342b311d8da15c690b
Irfan Hakim <irfanhakim.as@yahoo.com>
2025-08-19 12:37:18 +0800

    Sanitise during getting link instead of download

--- bin/main.py
+++ bin/main.py
@@ -120,6 +120,7 @@ def getUserInput(config, **kwargs):
         margin=colMargin,
         yes=getattr(args, "yes", None),
         list=getattr(args, "video", None),
+        sanitise=getConfigValue(config, "sanitise_links", default=True),
     )
 
     return {
@@ -136,7 +137,7 @@ def downloadVideos(config, queue, **kwargs):
     colMargin = kwargs.get("margin")
 
     # get config values
-    sanitiseLinks = getConfigValue(config, "sanitise_links", default=True)
+    # sanitiseLinks = getConfigValue(config, "sanitise_links", default=True)
 
     # get raw queue dicts
     sourceDict = getConfigValue(queue, "source")
@@ -151,7 +152,7 @@ def downloadVideos(config, queue, **kwargs):
     sourceOpts = getConfigValue(source, "opts")
     downloadPath = list(downloadPathDict.values())[0] if downloadPathDict else None
     cookiePath = list(cookieDict.values())[0] if cookieDict else None
-    videoList = sanitiseVideoList(videoList) if sanitiseLinks else videoList
+    # videoList = sanitiseVideoList(videoList) if sanitiseLinks else videoList
 
     # quit if any of the required values are missing
     if not (sourceOpts and downloadPath and videoList):
